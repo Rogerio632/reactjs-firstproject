8ced05e36101692f54674c5450cb42fd993047f8
Rogério Filho <rogeriofilho632@gmail.com>
2020-01-21 10:44:53 -0300

    feat: default container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: #fff;
+  max-width: 700px;
+  margin: 80px auto;
+  border-radius: 4px;
+  padding: 30px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    font-size: 21px;
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export default Container;
